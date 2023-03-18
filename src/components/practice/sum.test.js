0e3b73d9a32ad1d40e92ccf3069cdf08cd063daa
Anurag Kumar <anuragkumarrr@gmail.com>
2023-03-18 18:31:33 -0500

    Updated the file structure

--- src/components/practice/sum.test.js
+++ src/components/practice/sum.test.js
@@ -1,12 +1,19 @@
 import sum from './sum';
-
+describe('sum module 1', () => {
 it("when a & b is number",()=>{
     const result = sum(1,2)
     //result===3
     expect(result).toBe(3)
 })
-
+});
+describe('sum module 2', () => {
 it("when a &  b is string",()=>{
    const result = sum("1","2")
     expect(result).toBe(3)
 })
+});
+describe('sum module 3', () => {
+    test('adds 1 + 2 to equal 3', () => {
+      expect(sum(1, 2)).toBe(3);
+    });
+  });
